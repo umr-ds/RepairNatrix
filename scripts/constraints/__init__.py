eb2b2626f6dc4cf426df92149f2a60df94dad6aa
Marius Welzel <marius.welzel@gmx.de>
2022-07-21 10:22:39 +0200

    Removed redundant argument in constraint filtering.

--- scripts/constraints/__init__.py
+++ scripts/constraints/__init__.py
@@ -1,7 +1,7 @@
-#from gc_content import windowed_gc_content, overall_gc_content
-#from homopolymers import homopolymer
-#from kmer import kmer_counting
-#from undesired_subsequences import UndesiredSubSequenceFinder
-#uFinder = UndesiredSubSequenceFinder("undesired_subsequences.txt")
+from .gc_content import windowed_gc_content, overall_gc_content
+from .homopolymers import homopolymer
+from .kmer import kmer_counting
+from .undesired_subsequences import UndesiredSubSequenceFinder
+uFinder = UndesiredSubSequenceFinder("scripts/constraints/undesired_subsequences.txt") #scripts/constraints/
 
-#constraints = [homopolymer, uFinder.undesired_subsequences, overall_gc_content, windowed_gc_content, kmer_counting]
+constraints = [homopolymer, uFinder.undesired_subsequences, overall_gc_content, windowed_gc_content, kmer_counting]
